0d37b279e4fd841a56702021a8ce84dbc5573e4b
Cynrick Chiu <cyn.2k5@outlook.com>
2015-05-14 22:06:50 +0000

    Add cards.rb and image_blur_1.rb

---
+++ cards.rb
@@ -0,0 +1,49 @@
+class Card
+  attr_accessor :rank, :suit
+
+  def initialize(rank, suit)
+    @rank = rank
+    @suit = suit
+  end
+
+  def output_card
+    puts "#{self.rank} of #{self.suit}"
+  end
+end
+
+class Deck
+  def initialize
+    @cards = []
+    suits = %w{Spades Hearts Clubs Diamonds}
+    ranks = %w{Ace 2 3 4 5 6 7 8 9 10 Jack Queen King}
+
+    suits.each do |suit|
+      ranks.length.times do |i|
+        @cards << Card.new(ranks[i], suit)
+      end
+    end
+  end
+
+  def shuffle
+    @cards.shuffle!
+  end
+
+  def deal
+    # @cards.shift
+    top_card = @cards.shift
+    top_card.output_card
+  end
+
+  def output_deck
+    @cards.each { |card| card.output_card }
+  end
+
+  def deck_size
+    puts @cards.size
+  end
+end
+
+deck = Deck.new
+deck.shuffle
+deck.deal
+deck.deck_size
---
+++ image_blur_1.rb
@@ -0,0 +1,26 @@
+class Image
+  attr_accessor :arryas
+
+  def initialize(arrays)
+    @arrays = arrays
+  end
+
+  def output_image
+  	@arrays.each do |array|
+  	  array.size.times do |i|
+  	    print array[i]
+  	  end
+
+  	  puts
+  	end
+  end
+end
+
+image = Image.new([
+  [0, 0, 0, 0],
+  [0, 1, 0, 0],
+  [0, 0, 0, 1],
+  [0, 0, 0, 0]
+])
+
+image.output_image
