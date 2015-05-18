ef37273f65e6cb28a2a5d582c37a6924fc5b42da
Cynrick Chiu <cyn.2k5@outlook.com>
2015-05-18 21:44:17 +0000

    Rename variables

--- image_blur_1.rb
+++ image_blur_1.rb
@@ -17,26 +17,26 @@ class Image
 
   def blur
     pixels_indices = []
-    total_rows = self.rows.size           # total number of rows of image
-    elements_per_row = self.rows[0].size  # total number of elements per row of image
+    total_rows = self.rows.size         # total number of rows of image
+    total_elements = self.rows[0].size  # total number of elements per row of image
 
     # store indices of every 1s in pixels_indices
     self.rows.each_with_index do |row, row_index|
-      row.each_with_index do |pixel, pixel_index|
-        if pixel == 1
+      row.each_with_index do |element, pixel_index|
+        if element == 1
           pixels_indices << [row_index, pixel_index]
         end
       end
     end
 
-    pixels_indices.each do |element|
-      row = element[0]
-      index = element[1]
+    pixels_indices.each do |item|
+      row = item[0]
+      index = item[1]
       
       self.rows[row - 1][index] = 1 unless (row - 1) < 0
       self.rows[row + 1][index] = 1 unless (row + 1) >= total_rows
       self.rows[row][index - 1] = 1 unless (index - 1) < 0
-      self.rows[row][index + 1] = 1 unless (index + 1) >= elements_per_row
+      self.rows[row][index + 1] = 1 unless (index + 1) >= total_elements
     end
   end
 end
